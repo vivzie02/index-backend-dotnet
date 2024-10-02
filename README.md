e31fc59dd4e8a04994f4930903f7b4ae41d935c1
vivzie02 <101602263+vivzie02@users.noreply.github.com>
2024-10-02 18:56:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# index-backend-dotnet
+ 
---
+++ src/IO.Swagger/Models/DTOs/UserOutputDTO.cs
@@ -0,0 +1,26 @@
+﻿namespace IO.Swagger.Models.DTOs
+{
+    /// <summary>
+    /// UserOutputDTO
+    /// </summary>
+    public class UserOutputDTO
+    {
+        /// <summary>
+        /// Username
+        /// </summary>
+        public string Username { get; set; }
+        /// <summary>
+        /// Password
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public UserOutputDTO(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
